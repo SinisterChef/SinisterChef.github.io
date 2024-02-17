7fe402e1e39e9cfcd05e408d68e209e181617814
Kyle OBrien <kyleobis@gmail.com>
2024-02-16 23:15:15 -0500

    removing XMLHTTPrequest and updating with a fetch call

--- assets/dist/js/template.js
+++ assets/dist/js/template.js
@@ -1,25 +1,16 @@
 document.addEventListener("DOMContentLoaded", function() {
-    console.log("DOM loaded and parsed");
-    // Load header
-    var headerPlaceholder = document.getElementById("nav-reserve");
-    console.log("DOM loaded and parsed2");
-    var headerRequest = new XMLHttpRequest();
-    headerRequest.onload = function() {
-        if (headerRequest.status >= 200 && headerRequest.status < 400) {
-            headerPlaceholder.innerHTML = headerRequest.responseText;
-        }
-    };
-    headerRequest.open("GET", "./nav.html");
-    headerRequest.send();
 
-    // Load footer
-    var footerPlaceholder = document.getElementById("footer-reserve");
-    var footerRequest = new XMLHttpRequest();
-    footerRequest.onload = function() {
-        if (footerRequest.status >= 200 && footerRequest.status < 400) {
-            footerPlaceholder.innerHTML = footerRequest.responseText;
-        }
-    };
-    footerRequest.open("GET", "./footer.html");
-    footerRequest.send();
+    //header Load
+    fetch('./nav.html')
+        .then(response => response.text())
+        .then(data => {
+            document.getElementById("nav-reserve").innerHTML = data;
+        });
+
+    //footer Load
+    fetch('./footer.html')
+    .then(response => response.text())
+    .then(data => {
+        document.getElementById("footer-reserve").innerHTML = data;
+    });
 });
