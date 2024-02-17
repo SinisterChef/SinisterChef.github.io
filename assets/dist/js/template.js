ea2ecc57293e9f50f4f581b3f59412f377f38bcd
Kyle OBrien <kyleobis@gmail.com>
2024-02-16 22:56:23 -0500

    For fuck sake getelement doesnt like the template element

--- assets/dist/js/template.js
+++ assets/dist/js/template.js
@@ -1,9 +1,11 @@
 document.addEventListener("DOMContentLoaded", function() {
+    console.log("DOM loaded and parsed");
     // Load header
     var headerPlaceholder = document.getElementById("nav-reserve");
+    console.log("DOM loaded and parsed2");
     var headerRequest = new XMLHttpRequest();
     headerRequest.onload = function() {
-        if (headerRequest.status >= 200 && headerRequest.status < 300) {
+        if (headerRequest.status >= 200 && headerRequest.status < 400) {
             headerPlaceholder.innerHTML = headerRequest.responseText;
         }
     };
@@ -14,7 +16,7 @@ document.addEventListener("DOMContentLoaded", function() {
     var footerPlaceholder = document.getElementById("footer-reserve");
     var footerRequest = new XMLHttpRequest();
     footerRequest.onload = function() {
-        if (footerRequest.status >= 200 && footerRequest.status < 300) {
+        if (footerRequest.status >= 200 && footerRequest.status < 400) {
             footerPlaceholder.innerHTML = footerRequest.responseText;
         }
     };
